d102175b1388df8b2c42ab0a52e8bc0ad681be5f
delfile <delfile@WIN-TI8CUBJ4KFE>
2024-02-26 22:21:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleApp1/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Int a = 1;
+        a = ++a * a;
+        Console.WriteLine(a);
+    End Sub
+End Module
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string num1 = Console.ReadLine();
+        string num2 = Console.ReadLine();
+        string num3 = num1 + num2;
+        for (int i = 0; i < num1.Length; i++)
+        {
+            int Count = 0;
+            for (int j = 0; j < num3.Length; j++)
+            {
+                if (num1[i] == num3[j])
+                {
+                    Count++;
+                }
+            }
+            if (Count == 1)
+            {
+                Console.WriteLine($"Уникально число {num1[i]}");
+            }
+        }
+    }
+}
